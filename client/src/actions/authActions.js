286174a9f824b3b654fa80c5b548c9f9ea134589
Lahiru Rane <lahirurandika.ranasinghe@gmail.com>
2018-06-29 13:44:20 +1000

    added React routing and calling API for register and login using redux
    added React routing and calling API for register and login using redux

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+
+// Register User
+export const registeruser = (userData, history) => dispatch => {
+  axios
+    .post('/api/users/register', userData)
+    .then(res => history.push('/login'))
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+//from component to navigate
+//this.props.history.push('/dashboard');
+
+//Login - Get User token
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      //saves to local storage
+      const { token } = res.data;
+      //set token to local storage
+      localStorage.setItem('jwtToken', token);
+      //set token to Auth header
+      setAuthToken(token);
+      //Decode token to get user data
+      const decoded = jwt_decode(token);
+      //set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+//set Logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-const users = require("./routes/api/users");
-const posts = require("./routes/api/posts");
-const profile = require("./routes/api/profile");
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+const movies = require('./routes/api/movies');
 
 const app = express();
 
@@ -14,25 +15,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // connect to mongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log("Mongo ERROR RANE ", err));
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log('Mongo ERROR RANE ', err));
 
 // app.get('/', (req, res) => res.send('Hello Yo'))
 // Passport middleware
 app.use(passport.initialize());
 
 //Passport config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 // Use routes
-app.use("/api/users", users);
-app.use("/api/posts", posts);
-app.use("/api/profile", profile);
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+app.use('/api/movies', movies);
 
 const port = process.env.PORT || 5000;
 
